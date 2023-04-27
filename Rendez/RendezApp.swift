2b1bbe1d8f0ba12e8b02ddc2dd4d0673942c96a7
MiguelHernandezDexcom <112583722+MiguelHernandezDexcom@users.noreply.github.com>
2023-04-27 08:50:02 -0700

    Initial Commit

---
+++ Rendez/RendezApp.swift
@@ -0,0 +1,17 @@
+//
+//  RendezApp.swift
+//  Rendez
+//
+//  Created by Miguel Hernandez on 4/27/23.
+//
+
+import SwiftUI
+
+@main
+struct RendezApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

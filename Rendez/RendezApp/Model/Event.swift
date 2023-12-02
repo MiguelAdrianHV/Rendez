d3d9c59b64d7369a5a57a6b5c1d267423072d6a0
MiguelHernandezDexcom <112583722+MiguelHernandezDexcom@users.noreply.github.com>
2023-12-01 16:15:23 -0800

    data: Add event model

--- Rendez/RendezApp/Model/Event.swift
+++ Rendez/RendezApp/Model/Event.swift
@@ -9,9 +9,13 @@ import Foundation
 import SwiftUI
 
 struct Event {
-    var id: Int
+    var id: String
     var name: String
     var theme: String
-    var image: Image
-    var isFavorite: Bool
+    var description: String
+    var image: String
+}
+
+extension Event {
+    static var MOCK_EVENT = Event(id: NSUUID().uuidString, name: "Carrera 5Km", theme: "Deporte", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", image: "event-party-portada")
 }
